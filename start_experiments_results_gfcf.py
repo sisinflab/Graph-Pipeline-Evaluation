de046df00e03743d69b756ef3d1ce7490e2b5706
dmalitesta <d.malitesta@gmail.com>
2022-12-19 08:50:23 +0100

    Added results calculation for GFCF.

---
+++ start_experiments_results_gfcf.py
@@ -0,0 +1,12 @@
+from elliot.run import run_experiment
+import argparse
+
+parser = argparse.ArgumentParser(description="Run sample main.")
+parser.add_argument('--dataset', type=str, default='gowalla')
+parser.add_argument('--gpu', type=int, default=0)
+args = parser.parse_args()
+
+run_experiment(f"config_files/experiment_results_gfcf.yml",
+               dataset=args.dataset,
+               gpu=args.gpu,
+               config_already_loaded=False)
