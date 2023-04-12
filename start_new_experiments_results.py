80da9f9f2c9acbd8d43d147ca800799e00eff67e
dmalitesta <d.malitesta@gmail.com>
2023-04-12 12:51:00 +0200

    Reproducibility.

---
+++ start_new_experiments_results.py
@@ -0,0 +1,13 @@
+from elliot.run import run_experiment
+import argparse
+
+parser = argparse.ArgumentParser(description="Run sample main.")
+parser.add_argument('--dataset', type=str, default='allrecipes')
+parser.add_argument('--model', type=str, default='dgcf')
+parser.add_argument('--gpu', type=int, default=0)
+args = parser.parse_args()
+
+run_experiment(f"config_files/{args.model}_{args.dataset}.yml",
+               dataset=args.dataset,
+               gpu=args.gpu,
+               config_already_loaded=False)
