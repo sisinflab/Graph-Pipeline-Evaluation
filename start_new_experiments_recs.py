bea18b1b98079ec717653cd5a5e6e73228ab8355
dmalitesta <d.malitesta@gmail.com>
2023-04-14 07:09:29 +0200

    Reproducibility.

--- start_new_experiments_recs.py
+++ start_new_experiments_recs.py
@@ -3,11 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
 parser.add_argument('--dataset', type=str, default='allrecipes')
-parser.add_argument('--model', type=str, default='ngcf')
 parser.add_argument('--gpu', type=int, default=0)
 args = parser.parse_args()
 
-run_experiment(f"config_files/{args.model}_{args.dataset}.yml",
+run_experiment(f"config_files/experiment_quartiles.yml",
                dataset=args.dataset,
                gpu=args.gpu,
                config_already_loaded=False)
