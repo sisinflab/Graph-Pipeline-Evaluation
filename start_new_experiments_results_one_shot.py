6e3542d7d77f3fa93920649cbb865ea2975094a4
dmalitesta <d.malitesta@gmail.com>
2023-04-14 11:06:16 +0200

    Reproducibility.

--- start_new_experiments_results_one_shot.py
+++ start_new_experiments_results_one_shot.py
@@ -1,6 +1,6 @@
 from elliot.run import run_experiment
 
-run_experiment(f"config_files/one_shot_rp3beta.yml",
+run_experiment(f"config_files/one_shot_easer.yml",
                dataset='amazon-book',
                gpu=0,
                config_already_loaded=False)
